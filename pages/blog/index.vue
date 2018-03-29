ed72e9838097fd94b911f241876e8f11573cf184
mgochoa <mochoa1127@gmail.com>
2018-03-29 01:43:31 -0500

    init

---
+++ pages/blog/index.vue
@@ -0,0 +1,16 @@
+<template>
+<v-layout>
+    <div>blog</div>
+    <div>hola</div>
+</v-layout>
+</template>
+<script>
+export default {
+  
+}
+</script>
+<style>
+
+</style>
+
+
---
+++ pages/contact/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>Contacto</div>
+</template>
+<script>
+export default {
+  
+}
+</script>
+<style>
+
+</style>
